43d5758db67f5006f7d4467068a5fccfcb7c75b1
rohitsingh93300 <rohitsingh93300@gmail.com>
2024-02-03 13:04:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header"
+import ProductionHouse from "./components/ProductionHouse"
+import Slider from "./components/Slider"
+import './App.css'
+import GenreMovieList from "./components/GenreMovieList"
+
+
+function App() {
+
+
+  return (
+    <>
+    
+    <Header/>
+    <Slider/>
+    <ProductionHouse/>
+    <GenreMovieList/>
+  
+    </>
+  )
+}
+
+export default App
